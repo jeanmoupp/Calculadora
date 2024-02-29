3b04c99da753cb373cfedffb692777b2db06dbd1
unknown <jeanandradepasa@gmail.com>
2024-02-28 21:45:14 -0300

    Exercício de Calculadora de soma

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,60 @@
+
+ //*FAÇA UMA CALCULADORA QUE SOME SUBTRAIA MULTIPLIQUE E DIVIDA MOSTRANDO O RESULTADO NA TELA.
+
+package com.mycompany.calculadora;
+import java.util.Scanner;
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner cc = new Scanner (System.in);
+        
+        int a, b;
+        
+        System.out.println("Digite");        
+        a = cc.nextInt();
+        
+        System.out.println("Digite");        
+        b = cc.nextInt();
+        
+        System.out.println(a + b);
+        
+        cc.close();
+           
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
